88ec84ee89396d1003793a07415a8e2ae7e6388f
llwd123 <601584091@qq.com>
2016-07-06 15:52:26 +0800

    user

--- src/main/java/com/jeff/modules/dao/daoimpl/UserDaoImpl.java
+++ src/main/java/com/jeff/modules/dao/daoimpl/UserDaoImpl.java
@@ -1,5 +1,18 @@
 package com.jeff.modules.dao.daoimpl;
 
-public class UserDaoImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import com.jeff.common.persistence.BaseDao;
+import com.jeff.common.persistence.BaseDaoImpl;
+import com.jeff.modules.dao.UserDao;
+import com.jeff.modules.entity.User;
+
+@Repository("userDao")
+public class UserDaoImpl extends BaseDaoImpl<User> implements UserDao{
+	@Autowired
+	@Qualifier("baseDao")
+	private BaseDao baseDao;
 
 }
--- src/main/java/com/jeff/modules/entity/User.java
+++ src/main/java/com/jeff/modules/entity/User.java
@@ -1,5 +1,99 @@
 package com.jeff.modules.entity;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "tb_user", catalog = "freeform")
+public class User {
+	private String id;
+	private String userName;
+	private String password;
+	private String headPic;
+	private String mail;
+	private String registerTime;
+	
+	
+	
+	public User(){
+		
+	}
+	
+	public User(String id, String userName, String password, String headPic,
+			String mail, String registerTime) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.password = password;
+		this.headPic = headPic;
+		this.mail = mail;
+		this.registerTime = registerTime;
+	}
+	
+	
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid")
+	
+	@Column(name = "id", unique = true, nullable = false)
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	@Column(name = "user_name")
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	@Column(name = "password", nullable = false)
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Column(name = "head_pic")
+	public String getHeadPic() {
+		return headPic;
+	}
+
+	public void setHeadPic(String headPic) {
+		this.headPic = headPic;
+	}
+	
+	@Column(name = "mail", nullable = false)
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	@Column(name = "register_time", nullable = false)
+	public String getRegisterTime() {
+		return registerTime;
+	}
+
+	public void setRegisterTime(String registerTime) {
+		this.registerTime = registerTime;
+	}
+	
+	
+	
 }
