4aac88f6c58eacdd2a83b753f14013f493cde11f
jeffwcx <cunxuanwang@163.com>
2016-07-06 14:12:14 +0800

    mongodbDao modified and app.properties

--- src/main/java/com/jeff/common/mongodb/MongoDBDao.java
+++ src/main/java/com/jeff/common/mongodb/MongoDBDao.java
@@ -1,27 +1,47 @@
 package com.jeff.common.mongodb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoDatabase;
 
 @Component
 public class MongoDBDao {
-	
 	private static MongoClient instance = null;
-	@Value("#{appProperties['ip']}")
-	private static String ip = "123.206.209.13";
-	@Value("#{appProperties['port']}")
-	private static Integer port = 27017;
+	@Value("#{appProperties['mongodb.ip']}")
+	private static String ip;
+	@Value("#{appProperties['mongodb.port']}")
+	private static Integer port;
+	@Value("#{appProperties['mongodb.username']}")
+	private static String username;
+	@Value("#{appProperties['mongodb.password']}")
+	private static String password;
+	@Value("#{appProperties['mongodb.database']}")
+	private static String database;
 	
 	{
-		instance = new MongoClient(ip, port);
+		ServerAddress serverAddress = new ServerAddress(ip, port);
+		MongoCredential credential = MongoCredential
+				.createScramSha1Credential(username, database, password.toCharArray());
+		List<MongoCredential> credentials = new ArrayList<MongoCredential>();
+		credentials.add(credential);
+		instance = new MongoClient(serverAddress, credentials);
 	}
 	
 	public static MongoClient getClient() {
 		if(instance==null) {
-			instance = new MongoClient(ip, port);
+			ServerAddress serverAddress = new ServerAddress(ip, port);
+			MongoCredential credential = MongoCredential
+					.createScramSha1Credential(username, database, password.toCharArray());
+			List<MongoCredential> credentials = new ArrayList<MongoCredential>();
+			credentials.add(credential);
+			instance = new MongoClient(serverAddress, credentials);
 		}
 		return instance;
 	}
